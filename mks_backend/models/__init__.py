89668043018d55864c1fda469d3ba6eaf7152268
atimchenko <atimchenko@htsts.ru>
2020-07-31 11:37:07 +0300

    old filestorage deleted after protocol changes

--- mks_backend/models/__init__.py
+++ mks_backend/models/__init__.py
@@ -1,6 +1,12 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import event
+
 
 Base = declarative_base()
-
 DBSession = scoped_session(sessionmaker())
+
+
+@event.listens_for(DBSession, 'after_commit')
+def receive_after_commit(session):
+    print('Transaction was successful.')
