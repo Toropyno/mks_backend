e053c2961ecae509415f7974c48e2ba1bf9549aa
zaldamov <zaldamov@htsts.ru>
2020-08-03 18:45:36 +0300

    meetings update

--- mks_backend/models/meeting.py
+++ mks_backend/models/meeting.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, VARCHAR
 
 from mks_backend.models import Base
 
 
-class Meeting(Base):
+class Meetings_type(Base):
     __tablename__ = 'meeting'
     meetings_type_id = Column(Integer, primary_key=True, nullable=False)
+    fullname = Column(VARCHAR(255), nullable=False)
