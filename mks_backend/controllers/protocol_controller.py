8a7530021e6324dcebd193486882ad98b2e752f4
user <user@lnx-vdi-a168.aorti.ru>
2020-08-10 13:56:23 +0300

    MKSBRYANS-55: changed error messages

--- mks_backend/controllers/protocol_controller.py
+++ mks_backend/controllers/protocol_controller.py
@@ -93,8 +93,8 @@ class ProtocolControllerSchema(colander.MappingSchema):
                                        name='protocolNumber',
                                        validator=colander.Length(min=1,
                                                                  max=20,
-                                                                 min_err='Недопустимый номер протокола',
-                                                                 max_err='Недопустимый номер протокола'))
+                                                                 min_err='Слишком короткий номер протокола',
+                                                                 max_err='Слишком длинный номер протокола'))
     protocol_date = colander.SchemaNode(colander.String(),
                                         name='protocolDate',
                                         validator =date_validator)
@@ -106,14 +106,14 @@ class ProtocolControllerSchema(colander.MappingSchema):
                                         name='protocolName',
                                         validator=colander.Length(min=1,
                                                                   max=255,
-                                                                 min_err='Недопустимое имя протокола',
-                                                                 max_err='Недопустимое имя протокола'))
+                                                                 min_err='Слишком короткое имя протока',
+                                                                 max_err='Слишком длинное имя протокола'))
     note = colander.SchemaNode(colander.String(),
                                name='note',
                                validator=colander.Length(min=1,
                                                          max=2000,
-                                                         min_err='Недопустимое примечание',
-                                                         max_err='Недопустимое примечание'))
+                                                         min_err='Слишком короткое примечание',
+                                                         max_err='Слишком длинное примечание'))
     idfilestorage = colander.SchemaNode(colander.String(),
                                         name='idFileStorage',
                                         msg="Недопустимая информация о файле",
@@ -123,8 +123,8 @@ class ProtocolControllerFilterSchema(colander.MappingSchema):
     protocol_num = colander.SchemaNode(colander.String(), name='protocolNumber',
                                        validator=colander.Length(min=1,
                                                                  max=20,
-                                                                 min_err='Недопустимое имя протокола',
-                                                                 max_err='Недопустимое имя протокола'),
+                                                                 min_err='Слишком короткий номер протокола',
+                                                                 max_err='Слишком длинный номер протокола'),
                                        missing=None)
     meetings_type_id = colander.SchemaNode(colander.Int(),
                                            name='meeting',
@@ -134,8 +134,8 @@ class ProtocolControllerFilterSchema(colander.MappingSchema):
     protocol_name = colander.SchemaNode(colander.String(), name='protocolName',
                                         validator=colander.Length(min=1,
                                                                   max=255,
-                                                                 min_err='Недопустимое имя протокола',
-                                                                 max_err='Недопустимое имя протокола'),
+                                                                 min_err='Слишком короткое имя протокола',
+                                                                 max_err='Слишком длинное имя протокола'),
                                         missing=None)
     date_start = colander.SchemaNode(colander.String(), name='dateStart', validator=date_validator, missing=None)
     date_end = colander.SchemaNode(colander.String(), name='dateEnd', validator=date_validator, missing=None)
