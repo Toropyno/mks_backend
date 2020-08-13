c308f2f48a37e814adeea9a7f271903ff861b1d9
user <user@lnx-vdi-a155.aorti.ru>
2020-08-13 12:49:48 +0300

    MKSBRYANS-77: added API, added in router, added controllers

--- mks_backend/routes.py
+++ mks_backend/routes.py
@@ -4,3 +4,6 @@ def includeme(config):
     config.add_route('download_file', '/protocols/download/{uuid}')
     config.add_route('upload_file', '/protocols/upload')
     config.add_route('protocols_delete_change_and_view', '/protocols/{id}')
+
+    config.add_route('subcategories_lists', '/subcategories_lists')
+    config.add_route('add_subcategories_list', '/subcategories_lists/add')
