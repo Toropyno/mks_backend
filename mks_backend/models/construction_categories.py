75ecae51d19b8be03591f075cd0c06c3282ed366
atimchenko <atimchenko@htsts.ru>
2020-08-13 14:55:36 +0300

    MKSBRYANS-70: refactor and union all models, military_unit.py simplified;

--- mks_backend/models/construction_categories.py
+++ mks_backend/models/construction_categories.py
@@ -7,10 +7,15 @@ from mks_backend.models import Base
 class ConstructionCategories(Base):
 
     __tablename__ = 'construction_categories'
-    construction_categories_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    fullname = Column(VARCHAR(255), nullable=False)
+    construction_categories_id = Column(Integer, primary_key=True, autoincrement=True)
+    fullname = Column(VARCHAR(255), unique=True, nullable=False)
 
     subcategories_list = relationship(
         'SubcategoriesList',
         back_populates='construction_category'
+    )
+
+    construction = relationship(
+        'Construction',
+        back_populates='construction_categories'
     )
