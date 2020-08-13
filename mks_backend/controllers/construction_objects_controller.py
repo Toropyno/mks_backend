c6fe4a8178b6fc4d8ba72b6c127b08c02e309117
user <user@lnx-vdi-a168.aorti.ru>
2020-08-13 12:38:12 +0300

    MKSBRYANS-79: add controllers

--- mks_backend/controllers/construction_objects_controller.py
+++ mks_backend/controllers/construction_objects_controller.py
@@ -24,22 +24,22 @@ class ConstructionObjectsController(object):
     @view_config(route_name='construction_object', request_method='POST', renderer='json')
     def add_construction_object(self):
         pass
-        #construction_object_schema = ConstructionOjectControllerSchema()
-        #try:
-        #    construction_object_deserialized = construction_object_schema.deserialize(self.request.json_body)
-        #except colander.Invalid as error:
-        #    return Response(status=403, json_body=error.asdict())
-        #except ValueError as date_parse_error:
-        #    return Response(status=403, json_body=date_parse_error.args)
-        #construction_object = self.serializer.convert_schema_to_object(construction_object_deserialized)
-        #self.service.add_protocol(construction_object)
+        ##construction_object_schema = ConstructionOjectControllerSchema()
+        ##try:
+        ##    construction_object_deserialized = construction_object_schema.deserialize(self.request.json_body)
+        ##except colander.Invalid as error:
+        ##    return Response(status=403, json_body=error.asdict())
+        ##except ValueError as date_parse_error:
+        ##    return Response(status=403, json_body=date_parse_error.args)
+        #construction_object = self.serializer.get_construction_object_from_json(self.request.json_body)#convert_schema_to_object(construction_object_deserialized)
+        #self.service.add_construction_object(construction_object)
         #return {'id': construction_object.construction_object_id}
 
     @view_config(route_name='construction_objects_delete_change_and_view', request_method='GET', renderer='json')
     def get_construction_object(self):
         pass
         #id = self.request.matchdict['id']
-        #construction = self.service.get_construction_object_by_id(id)
+        #construction_object = self.service.get_construction_object_by_id(id)
         #json = self.serializer.convert_object_to_json(construction_object)
         #return json
 
@@ -53,15 +53,15 @@ class ConstructionObjectsController(object):
     @view_config(route_name='construction_objects_delete_change_and_view', request_method='PUT', renderer='json')
     def edit_construction_object(self):
         pass
-        #constructionobject__schema = ConstructionObjectControllerSchema()
         #id = self.request.matchdict['id']
-        #try:
-        #    construction_object_deserialized = construction_object_schema.deserialize(self.request.json_body)
-        #except colander.Invalid as error:
-        #    return Response(status=403, json_body=error.asdict())
-        #except ValueError as date_parse_error:
-        #    return Response(status=403, json_body=date_parse_error.args)
-        #construction_object_deserialized["id"] = id
-        #new_construction_object = self.serializer.convert_schema_to_object(construction_object_deserialized)
-        #new_construction_object = self.service.update_construction+object(new_construction_object)
-        #return {'id': new_construction_object.construction_object_id}
+        ##construction_object__schema = ConstructionObjectControllerSchema()
+        ##try:
+        ##    construction_object_deserialized = construction_object_schema.deserialize(self.request.json_body)
+        ##except colander.Invalid as error:
+        ##    return Response(status=403, json_body=error.asdict())
+        ##except ValueError as date_parse_error:
+        ##    return Response(status=403, json_body=date_parse_error.args)
+        ##construction_object_deserialized["id"] = id
+        #construction_object = self.serializer.get_construction_object_from_json(self.request.json_body) #convert_schema_to_object(construction_object_deserialized)
+        #construction_object = self.service.update_construction+object(construction_object)
+        #return {'id': construction_object.construction_object_id}
---
+++ mks_backend/controllers/object_categories_controller.py
@@ -0,0 +1,29 @@
+from pyramid.view import view_config
+
+#from mks_backend.services.object_categories_service import ConstructionStagesService
+#from mks_backend.serializers.object_categories_serializer import ConstructionStagesSerializer
+
+
+
+class ConstructionStagesController(object):
+
+    def __init__(self, request):
+        self.request = request
+        self.service = None #ObjectCategoriesService()
+        self.serializer = None #ObjectCategoriesSerializer()
+
+    @view_config(route_name='object_categories', request_method='GET', renderer='json')
+    def get_all_object_categories(self):
+        pass
+        #object_categories_array = self.service.get_all_object_categories_list()
+        #json = self.serializer.convert_list_to_json(object_categories_array)
+        #return json
+
+
+    @view_config(route_name='object_categories_view', request_method='GET', renderer='json')
+    def get_object_category(self):
+        pass
+        #id = self.request.matchdict['id']
+        #object_category = self.service.get_object_category_by_id(id)
+        #json = self.serializer.convert_object_to_json(object_category)
+        #return json
