f60ce00ecd7c7d4141e8bcef98a0ad595849ddb1
user <user@lnx-vdi-a155.aorti.ru>
2020-08-17 15:02:02 +0300

    MKSBRYANS-92: fix controllers

--- mks_backend/controllers/object_category_controller.py
+++ mks_backend/controllers/object_category_controller.py
@@ -4,47 +4,41 @@ from mks_backend.services.object_categories_service import ObjectCategoryService
 from mks_backend.serializers.object_category_serializer import ObjectCategorySerializer
 
 
-class ObjectCategoriesController(object):
+class ObjectCategoryController(object):
 
     def __init__(self, request):
         self.request = request
-        self.service =  ObjectCategoryService()
+        self.service = ObjectCategoryService()
         self.serializer = ObjectCategorySerializer()
 
     @view_config(route_name='object_categories', request_method='GET', renderer='json')
     def get_all_object_categories(self):
-        pass
-        #object_categories_array = self.service.get_all_object_categories_list()
-        #json = self.serializer.convert_list_to_json(object_categories_array)
-        #return json
-
+        object_categories_array = self.service.get_all_object_categories()
+        json = self.serializer.convert_list_to_json(object_categories_array)
+        return json
 
     @view_config(route_name='add_object_category', request_method='GET', renderer='json')
     def get_object_category(self):
-        pass
-        #id = self.request.matchdict['id']
-        #object_category = self.service.get_object_category_by_id(id)
-        #json = self.serializer.convert_object_to_json(object_category)
-        #return json
+        id = self.request.matchdict['id']
+        object_category = self.service.get_object_category_by_id(id)
+        json = self.serializer.convert_object_to_json(object_category)
+        return json
 
     @view_config(route_name='add_object_category', request_method='POST', renderer='json')
     def add_object_category(self):
-        pass
-        # object_category= self.service.get_object(self.request.json_body)
-        # object_category = self.service.add_object_category(object_categories_list)
-        # return {'id': object_category.object_category_id}
+        object_category = self.service.get_object(self.request.json_body)
+        object_category = self.service.add_object_category(object_category)
+        return {'id': object_category.object_category_id}
 
     @view_config(route_name='object_category_delete_change_and_view', request_method='DELETE', renderer='json')
     def delete_construction_object(self):
-        pass
-        # id = self.request.matchdict['id']
-        # self.service.delete_object_category_by_id(id)
-        # return {'id': id}
+        id = self.request.matchdict['id']
+        self.service.delete_object_category_by_id(id)
+        return {'id': id}
 
     @view_config(route_name='object_category_delete_change_and_view', request_method='PUT', renderer='json')
     def edit_object_categories_list(self):
-        pass
-        # id = self.request.matchdict['id']
-        # object_category = self.service.get_object(self.request.json_body)
-        # object_category = self.service.update_object_category(object_categories_list)
-        # return {'id': object_category.object_category_id}
+        id = self.request.matchdict['id']
+        object_category = self.service.get_object(self.request.json_body)
+        self.service.update_object_category(object_category)
+        return {'id': id}
