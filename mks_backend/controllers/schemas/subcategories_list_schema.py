4248625088bead34c42fadaa7c09ea0fca4eb660
imalyanova <user@lnx-vdi-a155.aorti.ru>
2020-08-19 18:48:12 +0300

    MKSBRYANS-89: transferred construction_subcategory_validator, construction_category_validator

--- mks_backend/controllers/schemas/subcategories_list_schema.py
+++ mks_backend/controllers/schemas/subcategories_list_schema.py
@@ -2,8 +2,6 @@ import colander
 
 from mks_backend.controllers.construction_controller import construction_category_validator
 from mks_backend.controllers.construction_controller import construction_subcategory_validator
-from mks_backend.repositories.construction_categories_repository import ConstructionCategoryRepository
-from mks_backend.repositories.construction_subcategories_repository import ConstructionSubcategoryRepository
 
 
 class SubcategoriesListSchema(colander.MappingSchema):
@@ -19,13 +17,3 @@ class SubcategoriesListSchema(colander.MappingSchema):
         name='constructionSubcategoriesId',
         validator=construction_subcategory_validator
     )
-
-
-def construction_category_validator(node, value):
-    if not ConstructionCategoryRepository.get_construction_category_by_id(value):
-        raise colander.Invalid(node, 'Такой категории для проекта не существует')
-
-
-def construction_subcategory_validator(node, value):
-    if not ConstructionSubcategoryRepository.get_construction_subcategory_by_id(value):
-        raise colander.Invalid(node, 'Такой подкатегории для проекта не существует')
--- mks_backend/controllers/schemas/validator_utils.py
+++ mks_backend/controllers/schemas/validator_utils.py
@@ -3,6 +3,9 @@ from datetime import datetime
 
 import colander
 
+from mks_backend.repositories.construction_categories_repository import ConstructionCategoryRepository
+from mks_backend.repositories.construction_subcategories_repository import ConstructionSubcategoryRepository
+
 
 def date_validator(node, value):
     try:
@@ -10,8 +13,19 @@ def date_validator(node, value):
     except ValueError:
         raise colander.Invalid(node, 'Неверный формат даты')
 
+
 def uuid_validator(node, value):
     pattern = '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[4][0-9a-fA-F]{3}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$'
     res = re.match(pattern, value)
     if res is None:
         raise colander.Invalid(node, 'Недопустимая информация о файле')
+
+
+def construction_category_validator(node, value):
+    if not ConstructionCategoryRepository.get_construction_category_by_id(value):
+        raise colander.Invalid(node, 'Такой категории для проекта не существует')
+
+
+def construction_subcategory_validator(node, value):
+    if not ConstructionSubcategoryRepository.get_construction_subcategory_by_id(value):
+        raise colander.Invalid(node, 'Такой подкатегории для проекта не существует')
