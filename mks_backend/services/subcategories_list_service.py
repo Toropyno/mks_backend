06c4b6cc261934986d0befa8196050ca78cb14f1
imalyanova <user@lnx-vdi-a155.aorti.ru>
2020-08-21 18:06:52 +0300

    MKSBRYANS-107: fix validation

--- mks_backend/services/subcategories_list_service.py
+++ mks_backend/services/subcategories_list_service.py
@@ -1,3 +1,4 @@
+from mks_backend.errors.db_basic_error import db_error_handler
 from mks_backend.repositories.subcategories_list_repository import SubcategoriesListRepository
 
 
@@ -9,6 +10,7 @@ class SubcategoriesListService:
     def get_subcategories_list_by_id(self, id):
         return self.repo.get_subcategories_list_by_id(id)
 
+    @db_error_handler
     def add_subcategories_list(self, subcategories_list):
         return self.repo.add_subcategories_list(subcategories_list)
 
@@ -17,3 +19,12 @@ class SubcategoriesListService:
 
     def get_all_subcategories_lists(self):
         return self.repo.get_all_subcategories_lists()
+
+    def get_uniq_error_message(self, uniq_error):
+        uniq_error = uniq_error.args[0]
+        u = ""
+        if "subcategories" in uniq_error:
+            u += " construction_subcategories_id"
+        if "_categories_id" in uniq_error:
+            u += " construction_categories_id"
+        return u
