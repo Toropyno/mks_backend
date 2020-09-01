0112d0597b83317eaafb59f889d27d9c3d3f54f7
IMalyanova <malyanova.ira@gmail.com>
2020-09-01 17:50:29 +0300

    MKSBRYANS-121: added object_categories in ZoneSerializer

--- mks_backend/serializers/zones_serializer.py
+++ mks_backend/serializers/zones_serializer.py
@@ -1,6 +1,6 @@
-from mks_backend.models.zones import Zones
-
 from mks_backend.errors.serilize_error import serialize_error_handler
+from mks_backend.models.zones import Zones
+from mks_backend.serializers.object_category_serializer import ObjectCategorySerializer
 
 
 class ZoneSerializer:
@@ -8,9 +8,14 @@ class ZoneSerializer:
     @classmethod
     @serialize_error_handler
     def convert_object_to_json(cls, zone):
+
         zone_dict = {
             'id': zone.zones_id,
             'fullName': zone.fullname,
+            'object_categories': [
+                ObjectCategorySerializer.convert_object_to_json(object_categories_list.object_categories_instance)
+                for object_categories_list in zone.object_categories_list
+            ],
         }
         return zone_dict
 
