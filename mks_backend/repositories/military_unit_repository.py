34c554926acdce847b364bdb75204e835745476f
astishenok <astishenok@htsts.ru>
2020-09-02 14:11:36 +0300

    MKSBRYANS-123: delete func, that not uses

--- mks_backend/repositories/military_unit_repository.py
+++ mks_backend/repositories/military_unit_repository.py
@@ -4,12 +4,9 @@ from mks_backend.repositories import DBSession
 
 class MilitaryUnitRepository:
 
-    def get_all_military_units(self):
-        return DBSession.query(MilitaryUnit).order_by(MilitaryUnit.idMU).all()
+    def get_root_military_units(self):
+        return DBSession.query(MilitaryUnit).filter(MilitaryUnit.pidMU == None)
 
     @classmethod
     def get_military_unit_by_id(cls, id):
         return DBSession.query(MilitaryUnit).get(id)
-
-    def get_root_military_units(self):
-        return DBSession.query(MilitaryUnit).filter(MilitaryUnit.pidMU == None)
