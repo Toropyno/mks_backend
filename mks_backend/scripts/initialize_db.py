ff3287045afab789edd61d0eb9dbc18dbdea167a
astishenok <astishenok@htsts.ru>
2020-09-02 13:35:03 +0300

    MKSBRYANS-123: return fallen after merge dev  imports to initialize_db.py

--- mks_backend/scripts/initialize_db.py
+++ mks_backend/scripts/initialize_db.py
@@ -1,5 +1,28 @@
 # Do not delete import models
+from mks_backend.models import Base
+from mks_backend.models.protocol import Protocol
 
+from mks_backend.models.filestorage import Filestorage
+from mks_backend.models.meeting import MeetingsType
+
+from mks_backend.models.commission import Commission
+from mks_backend.models.construction import Construction
+from mks_backend.models.construction_categories import ConstructionCategories
+from mks_backend.models.construction_objects import ConstructionObjects
+from mks_backend.models.construction_stages import ConstructionStages
+from mks_backend.models.construction_subcategories import ConstructionSubcategories
+from mks_backend.models.military_unit import MilitaryUnit
+from mks_backend.models.object_categories import ObjectCategories
+from mks_backend.models.object_categories_list import ObjectCategoriesList
+from mks_backend.models.subcategories_list import SubcategoriesList
+from mks_backend.models.zones import Zones
+
+from mks_backend.models.military_unit_models.combatarm import Combatarm
+from mks_backend.models.military_unit_models.keyword import Keyword
+from mks_backend.models.military_unit_models.militarycity import MilitaryCity
+from mks_backend.models.military_unit_models.namemilitaryunit import NameMilitaryUnit
+from mks_backend.models.military_unit_models.purposemu import PurposeMU
+from mks_backend.models.military_unit_models.sortarmedforces import SortArmedForces
 
 import os
 import sys
