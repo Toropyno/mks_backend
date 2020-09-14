d484ea6b72aa00239125880f5038d6b965b8ca96
astishenok <AStishenok@hthts.ru>
2020-09-14 16:36:26 +0300

    MKSBRYANS-123: change errors format for one type

--- mks_backend/errors/colander_error.py
+++ mks_backend/errors/colander_error.py
@@ -1,2 +1,2 @@
 def get_collander_error_dict(errors: dict) -> dict:
-    return {'code': 'validationError', 'message': '.\n\r'.join(errors.values())}
+    return {'code': 'validationError', 'message': '.\n\n'.join(errors.values())}
