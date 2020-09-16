ea07c11b3227806991401f6879669db9434671cf
IMalyanova <malyanova.ira@gmail.com>
2020-09-16 19:01:49 +0300

    MKSBRYANS-170: added object_document

--- mks_backend/controllers/object_document.py
+++ mks_backend/controllers/object_document.py
@@ -18,7 +18,7 @@ class ObjectDocumentController:
         self.service = ObjectDocumentService()
         self.schema = ObjectDocumentSchema()
 
-    @view_config(route_name='get_all_object_documents', request_method='GET', renderer='json')
+    @view_config(route_name='get_object_documents', request_method='GET', renderer='json')
     def get_all_object_documents(self):
         object_documents = self.service.get_all_object_documents()
         return self.serializer.convert_list_to_json(object_documents)
