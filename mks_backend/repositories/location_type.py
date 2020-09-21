01e91f5143bbb54a1383761592bbf859390a2f51
atimchenko <atimchenko@htsts.ru>
2020-09-21 11:14:41 +0300

    MKSBRYANS-171: refactoring - db_error_handler removed from services

--- mks_backend/repositories/location_type.py
+++ mks_backend/repositories/location_type.py
@@ -4,19 +4,22 @@ from mks_backend.repositories import DBSession
 
 class LocationTypeRepository:
 
+    def __init__(self):
+        self._query = DBSession.query(LocationType)
+
     def get_all_location_types(self) -> list:
-        return DBSession.query(LocationType).all()
+        return self._query.all()
 
     def add_location_type(self, location_type: LocationType) -> None:
         DBSession.add(location_type)
         DBSession.commit()
 
     def delete_location_type_by_id(self, id: int) -> None:
-        DBSession.query(LocationType).filter(LocationType.location_types_id == id).delete()
+        self._query.filter(LocationType.location_types_id == id).delete()
         DBSession.commit()
 
     def update_location_type(self, location_type: LocationType) -> None:
-        DBSession.query(LocationType).filter_by(location_types_id=location_type.location_types_id).update(
+        self._query.filter_by(location_types_id=location_type.location_types_id).update(
             {
                 'fullname': location_type.fullname,
             }
@@ -25,4 +28,4 @@ class LocationTypeRepository:
         DBSession.commit()
 
     def get_location_type_by_id(self, id: int) -> LocationType:
-        return DBSession.query(LocationType).get(id)
+        return self._query.get(id)
--- mks_backend/services/location_type.py
+++ mks_backend/services/location_type.py
@@ -1,4 +1,3 @@
-from mks_backend.errors.db_basic_error import db_error_handler
 from mks_backend.models.location_type import LocationType
 from mks_backend.repositories.location_type import LocationTypeRepository
 
@@ -13,11 +12,9 @@ class LocationTypeService:
     def get_location_type_by_id(self, id: int) -> LocationType:
         return self.repo.get_location_type_by_id(id)
 
-    @db_error_handler
     def add_location_type(self, location_type: LocationType) -> None:
         self.repo.add_location_type(location_type)
 
-    @db_error_handler
     def update_location_type(self, new_location_type: LocationType) -> None:
         self.repo.update_location_type(new_location_type)
 
