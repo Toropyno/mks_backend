14e129faaabec74624d7cecf7a2eb56cc117f289
IMalyanova <malyanova.ira@gmail.com>
2020-10-05 10:20:44 +0300

    added  construction_progress relationship

--- mks_backend/models/progress_status.py
+++ mks_backend/models/progress_status.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     Integer,
     VARCHAR
 )
+from sqlalchemy.orm import relationship
 
 from mks_backend.models import Base
 
@@ -12,3 +13,8 @@ class ProgressStatus(Base):
     __tablename__ = 'progress_statuses'
     progress_statuses_id = Column(Integer, primary_key=True)
     fullname = Column(VARCHAR(255), unique=True, nullable=False)
+
+    # construction_progress = relationship(
+    #     'ConstructionProgress',
+    #     back_populates='progress_statuses'
+    # )
