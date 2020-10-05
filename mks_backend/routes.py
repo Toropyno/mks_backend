68e4261b505d55e3510ffab9e86d7b529578b6cc
IMalyanova <malyanova.ira@gmail.com>
2020-10-05 12:00:01 +0300

    MKSBRYANS-175: added routes

--- mks_backend/routes.py
+++ mks_backend/routes.py
@@ -181,3 +181,8 @@ def includeme(config):
     config.add_route('edit_element_type', '/element_type/{id}', request_method='PUT')
     config.add_route('get_element_type', '/element_type/{id}', request_method='GET')
 
+    config.add_route('get_all_contract_statuses', '/contract_status', request_method='GET')
+    config.add_route('add_contract_status', '/contract_status', request_method='POST')
+    config.add_route('delete_contract_status', '/contract_status/{id}', request_method='DELETE')
+    config.add_route('edit_contract_status', '/contract_status/{id}', request_method='PUT')
+    config.add_route('get_contract_status', '/contract_status/{id}', request_method='GET')
