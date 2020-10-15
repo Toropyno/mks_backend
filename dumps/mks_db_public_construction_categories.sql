a68ef54429d89c85b291ed0aa0bf0216df021d7e
atimchenko <atimchenko@htsts.ru>
2020-10-15 15:22:16 +0300

    hotfix: new sql dumps added

--- dumps/mks_db_public_construction_categories.sql
+++ dumps/mks_db_public_construction_categories.sql
@@ -1,6 +1,5 @@
-INSERT INTO public.construction_categories (construction_categories_id, fullname) VALUES (28, 'Экспериментальная категория');
-INSERT INTO public.construction_categories (construction_categories_id, fullname) VALUES (35, 'Относительно новая категория');
+INSERT INTO public.construction_categories (construction_categories_id, fullname) VALUES (27, 'Наблюдательная категория');
+INSERT INTO public.construction_categories (construction_categories_id, fullname) VALUES (1, 'Заграничная категория');
+INSERT INTO public.construction_categories (construction_categories_id, fullname) VALUES (2, 'Ядерная категория');
 INSERT INTO public.construction_categories (construction_categories_id, fullname) VALUES (24, 'Гражданская категория');
 INSERT INTO public.construction_categories (construction_categories_id, fullname) VALUES (25, 'Военная категория');
-INSERT INTO public.construction_categories (construction_categories_id, fullname) VALUES (26, 'Исследовательская категория');
-INSERT INTO public.construction_categories (construction_categories_id, fullname) VALUES (27, 'Наблюдательная категория');
--- dumps/mks_db_public_construction_stages.sql
+++ dumps/mks_db_public_construction_stages.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.construction_stages (construction_stages_id, code, fullname) VALUES (3, 'end', 'Конец строительства');
 INSERT INTO public.construction_stages (construction_stages_id, code, fullname) VALUES (11, 'middle', 'Промежуточный этап');
+INSERT INTO public.construction_stages (construction_stages_id, code, fullname) VALUES (13, 'planning', 'Планирование строительства');
+INSERT INTO public.construction_stages (construction_stages_id, code, fullname) VALUES (3, 'end', 'Конец строительства');
 INSERT INTO public.construction_stages (construction_stages_id, code, fullname) VALUES (7, 'start', 'Начало строительства');
-INSERT INTO public.construction_stages (construction_stages_id, code, fullname) VALUES (13, 'very_start', 'Планирование строительства');
