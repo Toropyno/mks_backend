57f518f78a9c1e0acc19ac2598d11294e20b4591
atimchenko <atimchenko@htsts.ru>
2020-10-21 12:18:50 +0300

    protocol_and_filestorage_refactor: optional return type return in date_and_time.py added

--- mks_backend/serializers/utils/date_and_time.py
+++ mks_backend/serializers/utils/date_and_time.py
@@ -1,15 +1,15 @@
+from typing import Optional
+
 from datetime import date as Date
 from datetime import datetime as DateTime
 
 
-def get_date_string(date: Date) -> str:
-    if date:
-        return date.strftime('%Y,%m,%d')
+def get_date_string(date: Date) -> Optional[str]:
+    return date.strftime('%Y,%m,%d') if date else None
 
 
-def get_date_time_string(date_time: DateTime) -> str:
-    if date_time:
-        return date_time.strftime('%d.%m.%Y %H:%M:%S')
+def get_date_time_string(date_time: DateTime) -> Optional[str]:
+    return date_time.strftime('%d.%m.%Y %H:%M:%S') if date_time else None
 
 
 def get_date_time_zone(date_time: DateTime) -> str:
