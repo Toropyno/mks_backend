a5f5031080104e05103b4f76e95761fb370ae422
IMalyanova <IMalyanova@htsts.ru>
2020-11-05 19:00:40 +0300

    MKSBRYANS-236: added for zones-coordinates DB_ERROR_CODES

--- mks_backend/errors/db_error_codes.py
+++ mks_backend/errors/db_error_codes.py
@@ -4,41 +4,59 @@ DB_ERROR_CODES = {
     'commission_code_key_duplicate': 'Комиссия с указанным ключом уже существует!',
     'commission_fullname_key_duplicate': 'Комиссия с указанным именем уже существует!',
     'commission_pkey_duplicate': 'Комиссия с таким id уже существует!',
+
     'construction_project_code_key_duplicate': 'Проект с указанным ключом уже существует!',
     'construction_pkey_duplicate': 'Проект с таким id уже существует!',
+
     'construction_subcategories_fullname_key_duplicate': 'Подкатегория с указанным наименованием уже существует!',
     'construction_subcategories_pkey_duplicate': 'Подкатегория с таким id уже существует!',
+
     'construction_categories_fullname_key_duplicate': 'Категория с указанным наименованием уже существует!',
     'construction_categories_pkey_duplicate': 'Категория с таким id уже существует!',
+
     'location_types_fullname_key_duplicate': 'Тип Местоположения с таким названием уже существует!',
     'location_types_pkey_duplicate': 'Тип Местоположения с таким id уже существует!',
+
     'construction_companies_unique_duplicate': 'Организация с таким кратким и полным названием уже существует!',
     'construction_companies_pkey_duplicate': 'Организация с таким id уже существует!',
+
     'construction_objects_object_code_key_duplicate': 'Объект Строительства с таким кодом уже существует!',
     'construction_objects_pkey_duplicate': 'Объект Строительства с таким id уже существует!',
+
     'construction_stages_code_key_duplicate': 'Этап Строительства с таким кратким наименованием уже существует!',
     'construction_stages_fullname_key_duplicate': 'Этап Строительства с таким полным наименованием уже существует!',
     'construction_stages_pkey_duplicate': 'Этап Строительства с таким id уже существует!',
-    'object_categories_pkey_duplicate': 'Категории Объектов Строительства с таким id уже существуют!',
+
+    'object_categories_list_pkey_duplicate': 'Перечень Категорий Объектов с таким id уже существует!',
     'object_categories_list_unique_duplicate': 'Перечень Категорий Объектов с указанной Зоной Военного Городка и '
                                                'Категорией Объекта Строительства уже существует!',
+
+    'object_categories_pkey_duplicate': 'Категории Объектов Строительства с таким id уже существуют!',
     'object_categories_fullname_key_duplicate': 'Категория Объекта Строительства с таким наименованием уже '
                                                 'существует!',
+
     'zones_fullname_key_duplicate': 'Зона Военного Городка с таким наименованием уже существует!',
     'zones_pkey_duplicate': 'Зоны Военных Городков с таким id уже существуют!',
+
     'construction_types_fullname_key_duplicate': 'Такой Тип Проекта уже существует!',
     'construction_types_pkey_duplicate': 'Тип Проекта с таким id уже существует!',
+
     'leadership_positions_code_key_duplicate': 'Должность с таким кодом уже существует!',
     'leadership_positions_fullname_key_duplicate': 'Должность с таким наименованием уже существует!',
     'leadership_positions_pkey_duplicate': 'Должность с таким id уже существует!',
+
     'inspection_unique_duplicate': 'Поездка с такой датой и наименованием уже существует!',
     'inspection_files_pkey_duplicate': 'Файл Проверки с таким id уже существует!',
-    'fias_aoid_key_duplicate': 'FIAS с таким кодом уже существует!',
+
+    'fias_aoid_key_duplicate': 'FIAS с таким AOID уже существует!',
     'fias_pkey_duplicate': 'FIAS с таким id уже существует!',
+
     'realty_types_fullname_key_duplicate': 'Тип Недвижимости с таким наименованием уже существует!',
     'realty_types_pkey_duplicate': 'Тип Недвижимости с таким id уже существует!',
+
     'progress_statuses_fullname_duplicate': 'Статусы Хода Работ с таким наименованием уже существуют!',
     'progress_statuses_pkey_duplicate': 'Статусы Хода Работ с таким id уже существует!',
+
     'OKSM_code_key_duplicate': 'ОКСМ с таким кодом уже существует!',
     'OKSM_shortname_key_duplicate': 'ОКСМ с таким коротким именем уже существует!',
     'OKSM_alpha2_key_duplicate': 'ОКСМ с таким буквенным кодом Альфа-2 уже существует!',
@@ -51,6 +69,8 @@ DB_ERROR_CODES = {
     'subcategories_list_unique_duplicate': 'Перечень Подкатегорий с такими Категорией Проектов и Подкатегорией '
                                            'Проектов уже существует!',
     'subcategories_list_pkey_duplicate': 'Перечень Подкатегорий с таким id уже существует!',
+
+    'coordinates_pkey_duplicate': 'Координаты с таким id уже существуют!',
 
     'other_duplicate': 'Дубликат записи!',
 
