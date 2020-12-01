6f21aeabe0ce0a19fc567df4e9e4825cab72813c
atimchenko <atimchenko@htsts.ru>
2020-12-01 17:51:44 +0300

    bugfixes: inspection_file refactor: now files deleting by event

--- mks_backend/repositories/inspections/inspection_file.py
+++ mks_backend/repositories/inspections/inspection_file.py
@@ -1,11 +1,9 @@
 from typing import List
 
-from sqlalchemy import and_
-
 from mks_backend.models import DBSession
 from mks_backend.models.inspections.inspection_file import InspectionFile
 
-from mks_backend.errors.db_basic_error import db_error_handler
+from mks_backend.errors.db_basic_error import db_error_handler, DBBasicError
 
 
 class InspectionFileRepository:
@@ -21,9 +19,13 @@ class InspectionFileRepository:
     def get_files_by_inspection_id(self, id: int) -> List[InspectionFile]:
         return self._query.filter(InspectionFile.inspections_id == id).all()
 
-    def delete_inspection_file_by_id(self, inspection_id: int, file_id: str) -> None:
-        self._query.filter(and_(
-            InspectionFile.inspections_id == inspection_id,
-            InspectionFile.idfilestorage == file_id
-        )).delete()
+    def delete_inspection_file_by_id(self, idfilestorage: str) -> None:
+        inspection_file = self.get_inspection_file_by_id(idfilestorage)
+        DBSession.delete(inspection_file)
         DBSession.commit()
+
+    def get_inspection_file_by_id(self, idfilestorage: str):
+        inspection_file = self._query.get(idfilestorage)
+        if not inspection_file:
+            raise DBBasicError('inspection_file_nf')
+        return inspection_file
