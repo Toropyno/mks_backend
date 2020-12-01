d17ff45360b7b55605f46581d3a8f849120a53dc
atimchenko <atimchenko@htsts.ru>
2020-12-01 18:56:56 +0300

    bugfixes: swag event handler added

--- mks_backend/models/_events/main.py
+++ mks_backend/models/_events/main.py
@@ -2,6 +2,21 @@ from sqlalchemy import event
 from mks_backend.models import DBSession
 
 
+@event.listens_for(DBSession, 'before_commit')
+def receive_before_commit(session):
+    if session.new:
+        new_object = next(iter(session.new))
+        print('Trying to ADD {} to DB'.format(new_object))
+    elif session._deleted:
+        for instance in session._deleted.values():
+            print('Trying to DELETE {}'.format(instance))
+    elif session.identity_map:
+        edited_object = next(iter(session.identity_map._dict.keys()))[0]
+        print('Trying to EDIT {} in DB'.format(edited_object))
+    else:
+        print('Unknow event')
+
+
 @event.listens_for(DBSession, 'after_commit')
 def receive_after_commit(session):
-    print('Transaction was successful.')
+    print('Commit was successful.')
