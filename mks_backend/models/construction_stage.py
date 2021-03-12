ec9e8bc567a9535938fe6e0d4860fffe38c1462a
atimchenko <atimchenko@htsts.ru>
2021-03-12 17:25:23 +0300

    MKSBRYANS-291: construction stage columns improved

--- mks_backend/models/construction_stage.py
+++ mks_backend/models/construction_stage.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, VARCHAR
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, VARCHAR, ForeignKey, CheckConstraint
+from sqlalchemy.orm import relationship, backref
 
 from mks_backend.session import Base
 
@@ -10,8 +10,17 @@ class ConstructionStage(Base):
     construction_stages_id = Column(Integer, primary_key=True, autoincrement=True)
     code = Column(VARCHAR(20), unique=True, nullable=False)
     fullname = Column(VARCHAR(255), unique=True, nullable=False)
+    hierarchy_level = Column(Integer, CheckConstraint('hierarchy_level>0'), nullable=False, default=1)
+    ref_construction_stages_id = Column(ForeignKey(construction_stages_id, ondelete='SET NULL'))
 
     construction_object = relationship(
         'ConstructionObject',
         back_populates='construction_stage'
+    )
+
+    children = relationship(
+        'ConstructionStage',
+        backref=backref('parent', remote_side=construction_stages_id),
+        passive_deletes=True,
+        uselist=False
     )
--- mks_backend/serializers/construction_stage.py
+++ mks_backend/serializers/construction_stage.py
@@ -12,6 +12,11 @@ class ConstructionStageSerializer:
             'id': construction_stage.construction_stages_id,
             'code': construction_stage.code,
             'fullName': construction_stage.fullname,
+            'hierarchyLevel': construction_stage.hierarchy_level,
+            'parent': {
+                'id': construction_stage.parent.construction_stages_id,
+                'fullName': construction_stage.parent.fullname,
+            } if construction_stage.parent else None
         }
 
     def convert_list_to_json(self, construction_stages: list) -> list:
@@ -22,6 +27,8 @@ class ConstructionStageSerializer:
         if 'id' in schema:
             construction_stage.construction_stages_id = schema['id']
 
-        construction_stage.code = schema['code']
-        construction_stage.fullname = schema['fullName']
+        construction_stage.code = schema.get('code')
+        construction_stage.fullname = schema.get('fullName')
+        construction_stage.hierarchy_level = schema.get('hierarchyLevel')
+        construction_stage.ref_construction_stages_id = schema.get('parent')
         return construction_stage
