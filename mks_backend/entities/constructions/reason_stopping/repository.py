539bc40285779f8a3c522e7564b8c8e553af0c0a
nkavtkov <nkavtkov@htsts.ru>
2021-10-14 14:43:29 +0300

    MKSBRYANS-339: changed update and error processing

--- mks_backend/entities/constructions/reason_stopping/repository.py
+++ mks_backend/entities/constructions/reason_stopping/repository.py
@@ -1,6 +1,7 @@
 from .model import ReasonStopping
 
 from mks_backend.session import DBSession
+from mks_backend.errors import DBBasicError
 
 
 class ReasonStoppingRepository:
@@ -9,7 +10,7 @@ class ReasonStoppingRepository:
         self._query = DBSession.query(ReasonStopping)
 
     def get_all_reason_stoppings(self) -> list:
-        return self._query.all()
+        return self._query.order_by(ReasonStopping.fullname).all()
 
     def add_reason_stopping(self, reason_stopping: ReasonStopping) -> None:
         DBSession.add(reason_stopping)
@@ -20,13 +21,11 @@ class ReasonStoppingRepository:
         DBSession.commit()
 
     def update_reason_stopping(self, new_reason_stopping: ReasonStopping) -> None:
-        old_reason_stopping = self._query.filter_by(reason_stopping_id=new_reason_stopping.reason_stopping_id)
-        old_reason_stopping.update(
-            {
-                'fullname': new_reason_stopping.fullname,
-            }
-        )
-        DBSession.commit()
+        if DBSession.merge(new_reason_stopping) and not DBSession.new:
+            DBSession.commit()
+        else:
+            DBSession.rollback()
+            raise DBBasicError('reason_stopping_ad')
 
     def get_reason_stopping_by_id(self, id: int) -> ReasonStopping:
         return self._query.get(id)
