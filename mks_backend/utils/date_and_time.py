185ad23a9a80b3c992417eb44b507b780c733886
dpadushin <dpadushin@htsts.ru>
2022-03-16 16:24:17 +0300

    Revert "fix date comparison in litigation"
    This reverts commit 882b850a1b0d5e25822cce2144156c08a907d1a3

--- mks_backend/utils/date_and_time.py
+++ mks_backend/utils/date_and_time.py
@@ -21,11 +21,3 @@ def get_date_iso_string(date: Date) -> Optional[str]:
     :return: str строка даты в ISO формате, например: 2002-12-04
     """
     return date.isoformat() if date else None
-
-
-def get_date_time_from_string(date: str) -> Optional[Date]:
-    """
-    :param date: str строка даты в ISO формате, например: 2002-12-04
-    :return: DateTime
-    """
-    return DateTime.strptime(date, '%a %b %d %Y').date() if date else None
